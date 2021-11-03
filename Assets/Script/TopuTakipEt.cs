7feec180c9cb1421eeb7839a5153285ac2d8fb53
FatihHfz <fatih199832@hotmail.com>
2021-11-03 12:48:42 +0300

    camera following the ball

--- Assets/Script/TopuTakipEt.cs
+++ Assets/Script/TopuTakipEt.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TopuTakipEt : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform topunTransformu;
     void Start()
     {
         
@@ -13,6 +14,9 @@ public class TopuTakipEt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(topunTransformu.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x,topunTransformu.position.y,transform.position.z);
+        }
     }
 }
