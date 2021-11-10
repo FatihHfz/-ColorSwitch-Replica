0dffad7a7f415de1496f7c9ac9d06966df917859
FatihHfz <fatih199832@hotmail.com>
2021-11-10 21:25:50 +0300

    restart the scene

--- Assets/Script/TopKontrolu.cs
+++ Assets/Script/TopKontrolu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TopKontrolu : MonoBehaviour
 {
@@ -38,6 +39,7 @@ public class TopKontrolu : MonoBehaviour
         
         if(collision.tag != mevcutRenk)
         {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //SceneManager.LoadScene() da yazabilirdik.  
           Debug.Log("oldu");
           //  Destroy(gameObject);
         }
